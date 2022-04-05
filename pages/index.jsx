93764aea5f574eeafbe6628e9f15ce63b0a2dfe6
Pedro Mattos <phmattos0@gmail.com>
2022-04-05 09:51:25 -0300

    my first Next app

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Title from "../src/components/title";
+import style from "../src/style/style.module.css";
+
+function HomePage() {
+    const name = "Pedro";
+    return (
+   
+    <div><h1 className= {style.h1} >Bem vindo, {name}</h1>
+    <Title texto="Meusite" className={style.h1} />
+    </div>
+    );
+  }
+  
+export default HomePage
---
+++ src/components/title.jsx
@@ -0,0 +1,4 @@
+function Title({texto, className}){
+    return <h1 className={className}>{texto}</h1>;
+}
+export default Title;
