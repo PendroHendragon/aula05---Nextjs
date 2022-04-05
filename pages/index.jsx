0b78b15bd5fd51a6bf1de6da444012df14a70a62
Pedro Mattos <phmattos0@gmail.com>
2022-04-05 11:26:46 -0300

    changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,8 @@
-import Title from "../src/components/title";
-import style from "../src/style/style.module.css";
-
 function HomePage() {
   const name = "Pedro";
   return (
 
     <div><h1 className={style.h1} >Bem vindo, {name}</h1>
-      <Title texto="Meusite" className={style.h1} />
     </div>
   );
 }
